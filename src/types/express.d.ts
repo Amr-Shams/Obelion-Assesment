41b6046ff2d95450b13521b2da66f7f48ce9055c
Amr-Shams <amr.shams2015.as@gmail.com>
2024-10-19 10:05:13 +0300

    Update package.json and tsconfig.json

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,11 +1,8 @@
-import { User } from '@prisma/client';
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: User;
-    }
+declare namespace Express {
+  export interface Request {
+      user: any;
+  }
+  export interface Response {
+      user: any;
   }
 }
-
-export {};
