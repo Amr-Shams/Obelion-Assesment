cc2edb33f9cebf4b9ac7871a83e08c7181abe227
Amr-Shams <amr.shams2015.as@gmail.com>
2024-10-19 13:34:49 +0300

    Deploy to Railway

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,43 +3,45 @@ import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { isValidCredentials } from '../utils/validators';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const prisma = new PrismaClient();
 
 export const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  try {
-    const { email, password, name } = req.body;
+    try {
+        const { email, password, name } = req.body;
 
-    if (!isValidCredentials(email, password)) {
-      res.status(400).json({ error: 'Invalid email or password' });
-      return;
+        if (!isValidCredentials(email, password)) {
+            res.status(400).json({ error: 'Invalid email or password' });
+            return;
+        }
+
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        console.log("Process DatabaseURL: ", process.env.DATABASE_URL);
+        const user = await prisma.user.create({
+            data: { email, password: hashedPassword, name },
+        });
+
+        res.status(201).json(user);
+    } catch (error) {
+        next(error);
     }
-
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await prisma.user.create({
-      data: { email, password: hashedPassword, name },
-    });
-  
-    res.status(201).json(user);
-  } catch (error) {
-    next(error);
-  }
 };
 
 export const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const { email, password } = req.body;
-      const user = await prisma.user.findUnique({ where: { email } });
-  
-      if (!user || !(await bcrypt.compare(password, user.password))) {
-        res.status(401).json({ error: 'Invalid credentials' });
-        return;
-      }
-  
-      const token = jwt.sign({ id: user.id, email: user.email, isAdmin: user.isAdmin }, process.env.JWT_SECRET!, { expiresIn: '1h' });
-      res.status(200).json({ user, token });
+        const { email, password } = req.body;
+        const user = await prisma.user.findUnique({ where: { email } });
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            res.status(401).json({ error: 'Invalid credentials' });
+            return;
+        }
+
+        const token = jwt.sign({ id: user.id, email: user.email, isAdmin: user.isAdmin }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+        res.status(200).json({ user, token });
     } catch (error) {
-      next(error);
+        next(error);
     }
-  };
+};
--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -5,5 +5,6 @@ export const errorHandler = (err: any, req: Request, res: Response, next: NextFu
    res.status(400).json({ error: err.details[0].message });
     return;
   }
+  console.log(err);
   res.status(500).json({ error: 'Internal server error' });
 };
